d9106cdfb675b03e734134a71dbeb1567e1707bb
mor234 <moriya264@gmail.com>
2021-05-09 21:49:35 +0300

    first virson board

--- sources/Board.hpp
+++ sources/Board.hpp
@@ -3,24 +3,28 @@
 #include "Player.hpp"
 #include <map>
 #include <vector>
+#include<tuple> // for tuple
+
 
 #include <iostream>
 
 namespace pandemic{
     class Board{
         private:
-        std::map< City, std::pair <Color, std::vector <City> > >board;
-        void initialize_board();
-
+            static std::map< City, std::pair <Color, std::vector <City> > >board_city_map;
+            void initialize_board();
+            int sickness_cubes_num;
+            std::map<City,std::tuple<int /*sickness_cubes*/,int /*cures*/,int /*study_station*/>> city_attributes;
         public:
           
             Board ();
             void remove_cures();
             bool is_clean();
+            Color color_for_city(City city);
+
             //operator []
             const int & operator[](City city) const;
             int& operator[](City city);
-        
             //----------------------------------
             // friend global IO operators
             //----------------------------------
